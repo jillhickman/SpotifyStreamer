5ac48fe6e5643387d000a70e91caea96e62c6f8c
Jill Hickman <jillhickman@gmail.com>
2015-06-26 10:35:45 -0600

    Added toast to see what user entered

--- app/src/main/java/com/jillhickman/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/jillhickman/spotifystreamer/MainActivityFragment.java
@@ -1,12 +1,19 @@
 package com.jillhickman.spotifystreamer;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -56,6 +63,41 @@ public class MainActivityFragment extends Fragment {
         //Get reference to the ListView, and attach this adapter to it
         ListView listView = (ListView) rootView.findViewById(R.id.listview_search);
         listView.setAdapter(mResultAdapter);
+
+        //Using setOndEditorActionListener to listen for search actionSearch
+        //Getting the artist_search_edittext
+        //By making the editText final, I can use this inside inner classes.
+        final EditText editText = (EditText) rootView.findViewById(R.id.artist_search_edittext);
+        //Taking the editText and listening for input.
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    Context context = getActivity();
+                    //Gets the text from the editText, so can use Spotify for search
+                    CharSequence text = editText.getText();
+                    int duration = Toast.LENGTH_SHORT;
+
+//                    editText.clearFocus();
+//                    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(
+//                            Context.INPUT_METHOD_SERVICE);
+//                    imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+
+
+                    getActivity().getWindow().setSoftInputMode(
+                            WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+                    );
+
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
 
         return rootView;
     }
