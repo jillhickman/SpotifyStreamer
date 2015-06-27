ec94898112794ca94ff980321868e327d162924a
Jill Hickman <jillhickman@gmail.com>
2015-06-27 13:31:11 -0600

    Searched and got artist pager with a list to extract

--- app/src/main/java/com/jillhickman/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/jillhickman/spotifystreamer/MainActivityFragment.java
@@ -16,15 +16,17 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 
 /**
@@ -93,7 +95,29 @@ public class MainActivityFragment extends Fragment {
                             Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
 
+                    SpotifyApi api = new SpotifyApi();
 
+// Most (but not all) of the Spotify Web API endpoints require authorisation.
+// If you know you'll only use the ones that don't require authorisation you can skip this step
+//                    api.setAccessToken("myAccessToken");
+
+                    SpotifyService spotify = api.getService();
+
+                    spotify.searchArtists(text.toString(), new Callback<ArtistsPager>() {
+                        @Override
+                        public void success(ArtistsPager artistsPager, Response response) {
+                            Log.d("ArtistsPager success",artistsPager.toString());
+                            //Getting the list array from the artistPager, so I can
+                            //get artist name, SpotifyId, artist thumbnail image.
+                            List<Artist> artistList = artistsPager.artists.items;
+                            int n =0;
+                        }
+
+                        @Override
+                        public void failure(RetrofitError error) {
+                            Log.d("ArtistsPager failure", error.toString());
+                        }
+                    });
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
                     handled = true;
@@ -103,64 +127,7 @@ public class MainActivityFragment extends Fragment {
         });
 
 
-        // These two need to be declared outside the try/catch
-// so that they can be closed in the finally block.
-        HttpURLConnection urlConnection = null;
-        BufferedReader reader = null;
 
-// Will contain the raw JSON response as a string.
-        String forecastJsonStr = null;
-
-        try {
-            // Construct the URL for the OpenWeatherMap query
-            // Possible parameters are available at OWM's forecast API page, at
-            // http://openweathermap.org/API#forecast
-            URL url = new URL("https://api.spotify.com/v1/search?q=foo&type=artist");
-
-            // Create the request to OpenWeatherMap, and open the connection
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            // Read the input stream into a String
-            InputStream inputStream = urlConnection.getInputStream();
-            StringBuffer buffer = new StringBuffer();
-            if (inputStream == null) {
-                // Nothing to do.
-                forecastJsonStr = null;
-            }
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                // But it does make debugging a *lot* easier if you print out the completed
-                // buffer for debugging.
-                buffer.append(line + "\n");
-            }
-
-            if (buffer.length() == 0) {
-                // Stream was empty.  No point in parsing.
-                forecastJsonStr = null;
-            }
-            forecastJsonStr = buffer.toString();
-        } catch (IOException e) {
-            Log.e("PlaceholderFragment", "Error ", e);
-            // If the code didn't successfully get the weather data, there's no point in attempting
-            // to parse it.
-            forecastJsonStr = null;
-        } finally{
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (final IOException e) {
-                    Log.e("PlaceholderFragment", "Error closing stream", e);
-                }
-            }
-        }
         return rootView;
 
     }
