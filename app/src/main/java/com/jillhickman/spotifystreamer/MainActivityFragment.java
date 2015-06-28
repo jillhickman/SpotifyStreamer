0048a433c01522ec5a46c0737718765f36faad1b
Jill Hickman <jillhickman@gmail.com>
2015-06-27 22:09:55 -0600

    Added Async Task to get spotify search results to load all the way down to OnPostExecute. Left previous attempts of callback code for learning purposes.

--- app/src/main/java/com/jillhickman/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/jillhickman/spotifystreamer/MainActivityFragment.java
@@ -1,9 +1,9 @@
 package com.jillhickman.spotifystreamer;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +14,6 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,9 +23,6 @@ import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.Artist;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 
 /**
@@ -77,6 +73,7 @@ public class MainActivityFragment extends Fragment {
         //Getting the artist_search_edittext
         //By making the editText final, I can use this inside inner classes.
         final EditText editText = (EditText) rootView.findViewById(R.id.artist_search_edittext);
+
         //Taking the editText and listening for input.
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 
@@ -86,51 +83,96 @@ public class MainActivityFragment extends Fragment {
                 if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                     Context context = getActivity();
                     //Gets the text from the editText, so can use Spotify for search
-                    CharSequence text = editText.getText();
-                    int duration = Toast.LENGTH_SHORT;
+                    CharSequence artistName = editText.getText();
 
                     //Hides the keyboard after the enter key is pressed.
-//                    http://stackoverflow.com/questions/1109022/close-hide-the-android-soft-keyboard
+                    // reference from http://stackoverflow.com/questions/1109022/close-hide-the-android-soft-keyboard
                     InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(
                             Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
 
-                    SpotifyApi api = new SpotifyApi();
-
-// Most (but not all) of the Spotify Web API endpoints require authorisation.
-// If you know you'll only use the ones that don't require authorisation you can skip this step
-//                    api.setAccessToken("myAccessToken");
-
-                    SpotifyService spotify = api.getService();
-
-                    spotify.searchArtists(text.toString(), new Callback<ArtistsPager>() {
-                        @Override
-                        public void success(ArtistsPager artistsPager, Response response) {
-                            Log.d("ArtistsPager success",artistsPager.toString());
-                            //Getting the list array from the artistPager, so I can
-                            //get artist name, SpotifyId, artist thumbnail image.
-                            List<Artist> artistList = artistsPager.artists.items;
-                            int n =0;
-                        }
-
-                        @Override
-                        public void failure(RetrofitError error) {
-                            Log.d("ArtistsPager failure", error.toString());
-                        }
-                    });
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
                     handled = true;
+                    new FindArtistTask().execute(artistName.toString());
                 }
                 return handled;
             }
         });
-
-
-
         return rootView;
 
     }
+    //Set up AsyncTask (to start after the user execute the search)
+    private class FindArtistTask extends AsyncTask<String, Integer, List<Artist> > {
 
+        //member variable so that we can acess it outside of success method block
+        private List<Artist> mArtistList;
+
+        @Override
+        protected List<Artist> doInBackground(String... params) {
+
+            String artistName = params[0];
+            //Getting the handle for the SpotifyApi
+            SpotifyApi api = new SpotifyApi();
+
+            // Most (but not all) of the Spotify Web API endpoints require authorization.
+            // If you know you'll only use the ones that don't require authorisation you can skip this step
+            // api.setAccessToken("myAccessToken");
+
+            //Getting the Spotify Service
+            SpotifyService spotify = api.getService();
+
+//            spotify.searchArtists(artistName, new Callback<ArtistsPager>() {
+//                @Override
+//                public void success(ArtistsPager artistsPager, Response response) {
+//
+//                }
+//
+//                @Override
+//                public void failure(RetrofitError error) {
+//
+//                }
+//            });
+
+            //Executing the query, searching for artist
+            spotify.searchArtists(artistName);
+            ArtistsPager j = spotify.searchArtists(artistName);
+            mArtistList = j.artists.items;
+
+//            spotify.searchArtists(artistName, new Callback<ArtistsPager>() {
+//                @Override
+//                public void success(ArtistsPager artistsPager, Response response) {
+//                    Log.d("ArtistsPager success", artistsPager.toString());
+//
+//                    //Getting the list array from the artistPager, so I can
+//                    //get artist name, SpotifyId, artist thumbnail image.
+//                    mArtistList = artistsPager.artists.items;
+//                    int n =0;
+//                }
+//
+//                @Override
+//                public void failure(RetrofitError error) {
+//                    Log.d("ArtistsPager failure", error.toString());
+//                    //Toast to show that search failed in order to alert the user.
+//                    int duration = Toast.LENGTH_LONG;
+//                    Toast toast = Toast.makeText(getActivity(), "Artist cannot be found, Please search again", duration);
+//                    toast.show();
+//                }
+//            });
+            return mArtistList;
+        }
+
+//        @Override
+        //After doInBackground, call this method to update the view
+        protected void onPostExecute(List<Artist> artists) {
+            super.onPostExecute(artists);
+            mResultAdapter.clear();
+
+            //Do a for loop to extract the artist name & artist thumbnail image from List<Artist>
+//            for (Artist artist : artists){
+//
+//            }
+
+        }
+
+    }
 
 }
---
+++ app/src/main/java/com/jillhickman/spotifystreamer/SpotifyArtistAdapter.java
@@ -0,0 +1,7 @@
+package com.jillhickman.spotifystreamer;
+
+/**
+ * Created by jillhickman on 6/27/15.
+ */
+public class SpotifyArtistAdapter  {
+}
