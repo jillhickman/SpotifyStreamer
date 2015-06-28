318f029101c0ec4a6d07180f42474a9f44d636ce
Jill Hickman <jillhickman@gmail.com>
2015-06-27 23:35:05 -0600

    Added custom adapter and created a class. But added constructor but do not know if it's the right one.

--- app/src/main/java/com/jillhickman/spotifystreamer/SpotifyArtistAdapter.java
+++ app/src/main/java/com/jillhickman/spotifystreamer/SpotifyArtistAdapter.java
@@ -1,7 +1,17 @@
 package com.jillhickman.spotifystreamer;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+import kaaes.spotify.webapi.android.models.Artist;
+
 /**
  * Created by jillhickman on 6/27/15.
  */
-public class SpotifyArtistAdapter  {
+public class SpotifyArtistAdapter extends ArrayAdapter<Artist> {
+    public SpotifyArtistAdapter(Context context, int resource, List<Artist> objects) {
+        super(context, resource, objects);
+    }
 }
