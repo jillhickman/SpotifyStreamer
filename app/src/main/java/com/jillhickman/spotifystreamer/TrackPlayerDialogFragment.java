44b48145db78e8195d9ba8013137d405c7dc58bb
Jill Hickman <jillhickman@gmail.com>
2015-07-29 21:51:53 -0600

    Next button on dialog fragment updates the view and plays

--- app/src/main/java/com/jillhickman/spotifystreamer/TrackPlayerDialogFragment.java
+++ app/src/main/java/com/jillhickman/spotifystreamer/TrackPlayerDialogFragment.java
@@ -32,13 +32,11 @@ public class TrackPlayerDialogFragment extends DialogFragment{
     public Track mSongTrack;
 
     //Make it a member variable to manipulate the player.
-     public MediaPlayer mMediaPlayer = new MediaPlayer();
+    public MediaPlayer mMediaPlayer = new MediaPlayer();
 
+    //Int position of the track
+    private int mTrackPosition;
 
-//    static TrackPlayerDialogFragment newInstance() {
-//        TrackPlayerDialogFragment f = new TrackPlayerDialogFragment();
-//        return f;
-//    }
 
     //Inflate the fragment
     @Override
@@ -47,13 +45,14 @@ public class TrackPlayerDialogFragment extends DialogFragment{
 
         View v = inflater.inflate(R.layout.trackplayerlayout, container, false);
 
+        //Bundle from the TopTenTracksActivityFragment with position of track selected.
         Bundle mArgs = getArguments();
-        int position = mArgs.getInt("song");
+        mTrackPosition = mArgs.getInt("song");
 
         //Get handle to the Tracks list
         Tracks allTracks = DataRepo.trackListHolder;
         //Getting the handle for the Track obj in the appropriate position
-        mSongTrack= allTracks.tracks.get(position);
+        mSongTrack= allTracks.tracks.get(mTrackPosition);
 
         //Getting the handle to the imageView
         ImageView albumArtworkView = (ImageView) v.findViewById(R.id.trackplayer_image);
@@ -111,13 +110,74 @@ public class TrackPlayerDialogFragment extends DialogFragment{
 
 
         //Getting the handle to the previous button
+        Button previousButton = (Button) v.findViewById(R.id.trackplayer_previous_button);
+        previousButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                //Get the track url for current song playing
 
+//                String songUrl = mSongTrack.preview_url;
+            }
+        });
 
         //Getting the handle to the next button
+        Button nextButton = (Button) v.findViewById(R.id.trackplayer_next_button);
+        nextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                goNextTrack();
+            }
+        });
 
         //Getting the handle to the seek bar
 
         return v;
+    }
+    //Method to go to next track
+    private void goNextTrack(){
+        //If current track is playing, stop it.
+        if (mMediaPlayer.isPlaying()) {
+            mMediaPlayer.stop();
+        }
+            View v = getView();
+
+            //Get current position and increment to next position.
+            mTrackPosition++;
+            //Get handle to the Tracks list
+            Tracks allTracks = DataRepo.trackListHolder;
+            //Getting the handle for the Track obj in the appropriate position
+            mSongTrack= allTracks.tracks.get(mTrackPosition);
+
+            //Update the whole view to the new track
+            //Getting the handle to the imageView
+            ImageView albumArtworkView = (ImageView) v.findViewById(R.id.trackplayer_image);
+            //Get the album image for the song in the 0 position.
+            Image imageOfAlbumArtwork = mSongTrack.album.images.get(0);
+
+            //Get the Url of the image
+            String imageUrl = imageOfAlbumArtwork.url;
+            //Set picasso to use that Url
+            Picasso.with(getActivity()).load(imageUrl).into(albumArtworkView);
+
+            //Getting the handle to the artist name textView
+            TextView artistNameView =  (TextView) v.findViewById(R.id.trackplayer_artist_name);
+            //Getting the handle to the artist name from the DataRepo at selected position
+            Artist topTenArtist = DataRepo.topTenTrackArtist;
+            String artistName = topTenArtist.name;
+
+            //Set textView to the artistName
+            artistNameView.setText(artistName);
+
+            //Getting the handle to the album name textView
+            TextView albumNameView = (TextView) v.findViewById(R.id.trackplayer_albumn_name);
+            String albumName = mSongTrack.album.name;
+            //Set the textView to the album name
+            albumNameView.setText(albumName);
+
+            //Getting the handle to the track name textView
+            TextView  trackNameView = (TextView) v.findViewById(R.id.trackplayer_track_name);
+            String trackName = mSongTrack.name;
+            trackNameView.setText(trackName);
     }
 
 }
